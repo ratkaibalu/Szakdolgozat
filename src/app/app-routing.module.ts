88cd3dec7e85fb2a38ffd17939ae172d27cdca17
Ratkai <balazs.ratkai.ext@evosoft.com>
2023-09-05 14:24:55 +0200

    App init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LearningComponent } from './pages/learning/learning.component';
+import { TeamviewComponent } from './pages/teamview/teamview.component';
+import { TeamtableviewComponent } from './pages/teamtableview/teamtableview.component';
+import { TeammembersComponent } from './pages/teammembers/teammembers.component';
+import { TeammembersskillComponent } from './pages/teammembersskill/teammembersskill.component';
+import { LearningskillComponent } from './pages/learningskill/learningskill.component';
+import { LearningcategoryComponent } from './pages/learningcategory/learningcategory.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TeamviewComponent},
+  { path: 'learning', component: LearningComponent},
+  { path: 'learningskill', component: LearningskillComponent},
+  { path: 'learningcategory', component: LearningcategoryComponent},
+  { path: 'profile/:username', component: ProfileComponent},
+  { path: 'teammembers', component: TeammembersComponent},
+  { path: 'teammembersskill', component: TeammembersskillComponent},
+  { path: 'teamtableview', component: TeamtableviewComponent},
+  { path: '**', component: NotfoundComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LearningComponent } from './pages/learning/learning.component';
+import { TeamviewComponent } from './pages/teamview/teamview.component';
+import { TeamtableviewComponent } from './pages/teamtableview/teamtableview.component';
+import { TeammembersComponent } from './pages/teammembers/teammembers.component';
+import { TeammembersskillComponent } from './pages/teammembersskill/teammembersskill.component';
+import { LearningskillComponent } from './pages/learningskill/learningskill.component';
+import { LearningcategoryComponent } from './pages/learningcategory/learningcategory.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { InfobarComponent } from './components/infobar/infobar.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LearningComponent,
+    TeamviewComponent,
+    TeamtableviewComponent,
+    TeammembersComponent,
+    TeammembersskillComponent,
+    LearningskillComponent,
+    LearningcategoryComponent,
+    ProfileComponent,
+    NotfoundComponent,
+    InfobarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
