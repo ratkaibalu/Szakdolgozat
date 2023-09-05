86de652c263686c93b080241a1489824e748fc72
Ratkai <balazs.ratkai.ext@evosoft.com>
2023-09-05 14:26:05 +0200

    files added

---
+++ src/app/components/infobar/infobar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infobar',
+  templateUrl: './infobar.component.html',
+  styleUrls: ['./infobar.component.css']
+})
+export class InfobarComponent {
+
+}
